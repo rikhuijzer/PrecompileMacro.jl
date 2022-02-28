ad5edd59d13effb9c748d5e569725d11f648c523
Rik Huijzer <rikhuijzer@pm.me>
2022-02-28 12:38:18 +0100

    Drop MacroTools dependency (#5)
    * Drop MacroTools dependency
    
    * Type macOS instead of macos
    
    * Drop macOS
    
    * Optimize a bit

--- src/PrecompileMacro.jl
+++ src/PrecompileMacro.jl
@@ -1,25 +1,33 @@
 module PrecompileMacro
 
-using MacroTools: combinedef, splitarg, splitdef
-
 export @precompile
 
-"""
-Return tuple of types which can be passed as `args` into `precompile(f, args)`.
-"""
-function _types_tuple(def_dict)::Tuple{Vararg{Any}}
-    splitted = map(splitarg, def_dict[:args])
-    typesyms = Tuple(tup[2] for tup in splitted)::Tuple{Vararg{Symbol}}
-    # Convert, for example, `(:String, :Int)` to `(String, Int)`.
-    types = eval.(typesyms)
-    if !all(isconcretetype.(collect(types)))
-        nonconcrete = filter(!isconcretetype, types)
-        mult = 1 < length(nonconcrete)
-        msg = "The type$(mult ? 's' : "") $nonconcrete in the signature $types $(mult ? "are" : "is") not concrete."
-        error(msg)
+function _precompile(func::Expr)
+    func_signature = func.args[1]::Expr
+    func_signature.head == :where && error("PrecompileMacro.@precompile is not implemented for methods with type parameters")
+
+    signature_args = func_signature.args::Vector{Any}
+    func_name = signature_args[1]::Symbol
+
+    # Drop function name and kwargs.
+    args = convert(Vector{Expr}, filter(x -> x isa Expr && x.head != :parameters, signature_args))
+    types = Tuple(eval(last(arg.args))::DataType for arg in args)
+    for type in types
+        if !isconcretetype(type)
+            error("The type $type in the signature $(func_name)$(types) is not concrete")
+        end
     end
-    return types
+
+    precompile_ex = :(precompile($func_name, $types))
+
+    # Note that `precompile` returns a boolean to indicate whether a statement is active.
+    # Might be useful at some point.
+    return esc(quote
+        Base.@__doc__($func)
+        $precompile_ex
+    end)
 end
+precompile(_precompile, (Expr,))
 
 """
     @precompile(func)
@@ -28,22 +36,7 @@ Define function `func` and define a precompile statement for `func`, see `precom
 Return whether a precompile statement is active.
 """
 macro precompile(func)
-    def_dict = try
-        splitdef(func)
-    catch
-        error("@precompile must be applied to a method definition")
-    end
-
-    name = def_dict[:name]
-    precompile_args = _types_tuple(def_dict)
-    precompile_ex = :(precompile($name, $precompile_args))
-
-    # Note that `precompile` returns a boolean to indicate whether a statement is active.
-    # Might be useful at some point.
-    return esc(quote
-        Base.@__doc__($func)
-        $precompile_ex
-    end)
+    return _precompile(func)
 end
 
 end # module
